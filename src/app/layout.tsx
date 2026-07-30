bfc30fb93404508a2f32f1107a4d8c0d6a2e666e
nuruzaman milon <njmilon1@gmail.com>
2026-07-30 13:39:30 +0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      data-scroll-behavior="smooth"
       className={`${geistSans.variable} ${geistMono.variable} ${fraunces.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
